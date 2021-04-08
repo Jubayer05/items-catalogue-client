dd1b67a3f6052d2baff0afeb3cb48fcb690d2848
Jubayer05 <jubayer0504@gmail.com>
2021-04-08 09:08:41 +0600

    after deploying backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/items';
+const url = 'https://items-catalogue.herokuapp.com/items';
 
 export const fetchItems = () => axios.get(url);
 export const createItems = (newPost) => axios.post(url, newPost);
